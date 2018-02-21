878323da81190773f2075bb6efea1a32e4040df4
AlexRope <AlexRope@null>
2018-02-21 17:51:57 +0000

    Seperated header from source.

--- estim.c
+++ estim.c
@@ -1,32 +1,9 @@
 /*
-E-Stim Command Line Control
+E-Stim Command Line Tool for 2B
 Author: AlexRope <https://fetlife.com/users/4887759>
 */
 
-#include <errno.h>
-#include <fcntl.h> 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <termios.h>
-#include <unistd.h>
-
-typedef struct {
-	int battery;
-	int channel_a;
-	int channel_b;
-	int duty_cycle;
-	int feeling;
-	int mode;
-	char power;
-	int joined;
-	char version[5];
-} Status;
-
-typedef enum emode { MODE_PULSE, MODE_BOUNCE, MODE_CONTINUOUS, MODE_ASPLIT, MODE_BSPLIT, MODE_WAVE, MODE_WATERFALL, MODE_SQUEEZE, MODE_MILK, MODE_THROB, MODE_THRUST, MODE_RANDOM, MODE_STEP, MODE_TRAINING, MODE_MICROPHONE, MODE_STEREO, MODE_TICKLE } emode_t;
-typedef enum channel { CHANNEL_A, CHANNEL_B } channel_t;
-typedef enum power { POWER_LOW, POWER_HIGH } power_t;
-typedef enum joined { CHANNELS_UNLINKED, CHANNELS_LINKED } joined_t;
+#include "estim.h"
 
 const char* STR_MODES[] = { "PULSE", "BOUNCE", "CONTINUOUS", "A_SPLIT", "B_SPLIT", "WAVE", "WATERFALL", "SQUEEZE", "MILK", "THROB", "THRUST", "RANDOM", "STEP", "TRAINING", "MICROPHONE", "STEREO", "TICKLE" };
 const char* STR_JOINED[] = { "CHANNELS_UNLINKED", "CHANNELS_LINKED" };
